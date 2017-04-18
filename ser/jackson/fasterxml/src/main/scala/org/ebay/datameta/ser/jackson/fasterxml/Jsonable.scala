9790759eba2fe610ca5102a7e780a964d169f55e
Michael Bergens <mubergens@ebay.com>
2017-04-18 16:14:30 -0700

    2.0.0 release: added conditional ignoring of unknown JSON fields.

--- ser/jackson/fasterxml/src/main/scala/org/ebay/datameta/ser/jackson/fasterxml/Jsonable.scala
+++ ser/jackson/fasterxml/src/main/scala/org/ebay/datameta/ser/jackson/fasterxml/Jsonable.scala
@@ -25,11 +25,15 @@ trait Jsonable[T] {
 
   /**
     * Reads into the given object
+    *
+    * Defaults to ignoring unknown fields.
     */
-  def read(source: JsonParser, into: T): T
+  def readInto(source: JsonParser, target: T, ignoreUnknown: Boolean = true): T
 
   /**
-    * Creates a new instance of `T` and delegates to `read(source: JsonParser, into: T): T`
+    * Creates a new instance of `T` and delegates to `read(source: JsonParser, into: T): T`.
+    *
+    * Defaults to ignoring unknown fields.
     */
-  def read(source: JsonParser): T
+  def read(source: JsonParser, ignoreUnknown: Boolean = true): T
 }
